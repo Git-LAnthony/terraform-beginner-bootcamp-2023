3a26a5f4b4106613c092aeeb95136dff21337bfc
Git-LAnthony <45934844+Git-LAnthony@users.noreply.github.com>
2023-10-08 18:57:57 +0100

    21 restructure root module (#22)
    * Update week1.md
    
    * Update week1.md
    
    * #21 Restructure Root module

--- main.tf
+++ main.tf
@@ -1,33 +1,3 @@
-terraform {
-
-  cloud {
-    organization = "ResumeApp"
-
-    workspaces {
-      name = "terra_house_1"
-    }
-  }
-
-  required_providers {
-    random = {
-      source  = "hashicorp/random"
-      version = "3.5.1"
-    }
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.19.0"
-    }
-  }
-}
-
-provider "aws" {
-  # Configuration options
-}
-
-provider "random" {
-  # Configuration options
-}
-
 resource "random_string" "bucket_name" {
   lower   = true
   upper   = false
@@ -38,8 +8,9 @@ resource "random_string" "bucket_name" {
 
 resource "aws_s3_bucket" "example" {
   bucket = random_string.bucket_name.result
+
+   tags = {
+    UserUuid       = var.user_uuid
+  }
 }
 
-output "random_bucket_name" {
-  value = random_string.bucket_name
-}
