d790c6b075255b4436196e20662cfa2b19466c86
Git-LAnthony <45934844+Git-LAnthony@users.noreply.github.com>
2023-10-09 21:22:16 +0100

    31 setup content version (#32)
    * Update week1.md
    
    * #31 setup content validation for html file

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -36,6 +36,10 @@ resource "aws_s3_object" "index_html" {
   source = var.index_html_filepath
 
   etag = filemd5(var.index_html_filepath)
+  lifecycle {
+    replace_triggered_by = [terraform_data.content_version.output]
+    ignore_changes = [etag]
+  }
 }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
@@ -45,6 +49,10 @@ resource "aws_s3_object" "error_html" {
   source = var.error_html_filepath
 
   etag = filemd5(var.error_html_filepath)
+}
+
+resource "terraform_data" "content_version" {
+  input = var.content_version
 }
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
