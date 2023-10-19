baa5bada711b55c5798e9b1f43e6c8d15b78b510
Git-LAnthony pod <Lamaianthony@gmail.com>
2023-10-19 19:59:25 +0000

    #51 deploy to terratown test env

--- main.tf
+++ main.tf
@@ -23,9 +23,9 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567/api"
-  user_uuid="0810103b-1b56-4657-afdf-401d1c2b9cc0" 
-  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+  endpoint = var.terratowns_endpoint
+  user_uuid = var.teacherseat_user_uuid
+  token = var.terratowns_access_token
 }
 # resource "random_string" "bucket_name" {
 #   lower   = true
@@ -35,15 +35,14 @@ provider "terratowns" {
 
 # }
 
-# module "terrahouse_aws" {
-#   source = "./modules/terrahouse_aws"
-#   user_uuid = var.user_uuid
-#   bucket_name = var.bucket_name
-#   index_html_filepath = var.index_html_filepath
-#   error_html_filepath = var.error_html_filepath
-#   content_version = var.content_version
-#   assets_path = var.assets_path
-# }
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.teacherseat_user_uuid
+  index_html_filepath = var.index_html_filepath
+  error_html_filepath = var.error_html_filepath
+  content_version = var.content_version
+  assets_path = var.assets_path
+}
 
 resource "terratowns_home" "home" {
   name = "My fav food"
@@ -53,8 +52,8 @@ often flavored with soy sauce or miso, and topped with ingredients such as slice
 It is a popular and versatile dish with various regional variations and toppings, offering a satisfying and flavorful experience. 
 Ramen has gained international popularity and is enjoyed for its rich broth, chewy noodles, and diverse range of accompanying ingredients.
 DESCRIPTION
-  #domain_name = module.terrahouse_aws.cloudfront_url
-  domain_name = "3fdq3gz.cloudfront.net"
+  domain_name = module.terrahouse_aws.cloudfront_url
+ # domain_name = "3fdq3gz.cloudfront.net"
   town = "Cooker Crove"
   content_version = 1
 }
