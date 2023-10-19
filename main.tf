af347b63e5619f60936e6090873ae5d6f6969ae6
Git-LAnthony <45934844+Git-LAnthony@users.noreply.github.com>
2023-10-19 08:21:57 +0100

    #49 finish working on terraform provider (#50)

--- main.tf
+++ main.tf
@@ -44,3 +44,17 @@ provider "terratowns" {
 #   content_version = var.content_version
 #   assets_path = var.assets_path
 # }
+
+resource "terratowns_home" "home" {
+  name = "My fav food"
+  description = <<DESCRIPTION
+Ramen is a Japanese dish that consists of Chinese-style wheat noodles served in a meat or fish-based broth, 
+often flavored with soy sauce or miso, and topped with ingredients such as sliced pork, dried seaweed, green onions, and a half-boiled egg. 
+It is a popular and versatile dish with various regional variations and toppings, offering a satisfying and flavorful experience. 
+Ramen has gained international popularity and is enjoyed for its rich broth, chewy noodles, and diverse range of accompanying ingredients.
+DESCRIPTION
+  #domain_name = module.terrahouse_aws.cloudfront_url
+  domain_name = "3fdq3gz.cloudfront.net"
+  town = "Cooker Crove"
+  content_version = 1
+}
